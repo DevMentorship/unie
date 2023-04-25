146bd1e3073eaa56592aeb54554aa11cfaa8c234
Liia Zubova <vardlitmo@gmail.com>
2023-04-25 15:10:01 +0300

    //

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,24 +1,23 @@
 import 'swiper/css';
 import 'swiper/css/bundle';
-import './Slider.module.css';
 
 import Image from 'next/image';
-import React, { useRef, useState } from 'react';
 import { A11y, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+import styles from '@/components/Slider/Slider.module.css';
 
 export const Slider = () => (
   <Swiper
     // install Swiper modules
-    modules={[Pagination]}
+    modules={[Pagination, A11y]}
     spaceBetween={30}
     slidesPerView={4}
     pagination={{ clickable: true }}
     // centeredSlides={true}
     className="mySwiper"
   >
-    <SwiperSlide>
-      {/* <p className="paragraph2">
+    {/* <p className="paragraph2">
         Если вы увидите парня в Unie, который всегда рад вас видеть, всегда готов помочь с выбором и подсказать, какой
         кофе сегодня на фильтре, а какое пиво на кране, то знайте, что это Никита!
       </p>
@@ -30,24 +29,18 @@ export const Slider = () => (
         <button>VK</button>
       </div>
     </SwiperSlide> */}
+    <div className={styles.team}>
+      <SwiperSlide>
+        <div className={styles.team_blocks}>
+          <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
+        </div>
+      </SwiperSlide>
 
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.team_blocks}>
+          <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
+        </div>
+      </SwiperSlide>
+    </div>
   </Swiper>
 );
