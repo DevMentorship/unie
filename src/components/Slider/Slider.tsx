27143fe415c63407593acc5f3d9042c0ba78cc58
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-04-25 11:05:02 +0400

    Clean up a bit

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,53 +1,56 @@
 import 'swiper/css';
 import 'swiper/css/bundle';
-import './Slider.module.css';
 
 import Image from 'next/image';
-import React, { useRef, useState } from 'react';
-import { A11y, Pagination } from 'swiper';
+import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-export const Slider = () => (
-  <Swiper
-    // install Swiper modules
-    modules={[Pagination]}
-    spaceBetween={30}
-    slidesPerView={4}
-    pagination={{ clickable: true }}
-    // centeredSlides={true}
-    className="mySwiper"
-  >
-    <SwiperSlide>
-      {/* <p className="paragraph2">
-        Если вы увидите парня в Unie, который всегда рад вас видеть, всегда готов помочь с выбором и подсказать, какой
-        кофе сегодня на фильтре, а какое пиво на кране, то знайте, что это Никита!
-      </p>
-      <button>Подробнее</button>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-      <div className={styles.soсials}>
-        <h2 className="h2">Никита</h2>
-        <button>instagram</button>
-        <button>VK</button>
-      </div>
-    </SwiperSlide> */}
+import styles from '@/components/Slider/Slider.module.css';
 
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
+export const Slider = () => {
+  const slides = [
+    {
+      name: 'Никита',
+      description:
+        'Если вы увидите парня в Unie, который всегда рад вас видеть, всегда готов помочь с выбором и подсказать, какой кофе сегодня на фильтре, а какое пиво на кране, то знайте, что это Никита!',
+      instagram: 'instagram',
+      vk: 'vk',
+      photo: '/nikita.jpg',
+    },
+    {
+      name: 'Никита',
+      description:
+        'Если вы увидите парня в Unie, который всегда рад вас видеть, всегда готов помочь с выбором и подсказать, какой кофе сегодня на фильтре, а какое пиво на кране, то знайте, что это Никита!',
+      instagram: 'instagram',
+      vk: 'vk',
+      photo: '/nikita.jpg',
+    },
+    {
+      name: 'Никита',
+      description:
+        'Если вы увидите парня в Unie, который всегда рад вас видеть, всегда готов помочь с выбором и подсказать, какой кофе сегодня на фильтре, а какое пиво на кране, то знайте, что это Никита!',
+      instagram: 'instagram',
+      vk: 'vk',
+      photo: '/nikita.jpg',
+    },
+  ];
 
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-    <SwiperSlide>
-      <Image src={'/nikita.jpg'} alt={'nikitas photo'} width={270} height={360} />
-    </SwiperSlide>
-  </Swiper>
-);
+  return (
+    <Swiper
+      modules={[Pagination]}
+      spaceBetween={30}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      centeredSlides={true}
+      className={styles.slider}
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <p className="paragraph2">{slide.description}</p>
+          <button>Подробнее</button>
+          <Image src={slide.photo} alt={slide.name} width={270} height={360} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
