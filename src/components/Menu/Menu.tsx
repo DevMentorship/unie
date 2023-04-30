8c6254df73f6929d0bc2eca3dfaa64613cd46f97
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-04-30 09:50:28 +0400

    Clean up a bit

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+import { urlFor } from '@/lib/client';
+
+export interface IMenu {
+  _id: string;
+  mainImage: string;
+  description: string;
+}
+
+export interface IProps {
+  menu: IMenu[];
+}
+
+export const Menu = ({ menu }: IProps) => (
+  <section>
+    <h2>Menu</h2>
+    <ul>
+      {menu?.map(({ _id, mainImage, description }: IMenu) => (
+        <li key={_id}>
+          <Image src={urlFor(mainImage).url()} width="210" height="280" alt="" />
+          <h2>{description}</h2>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
