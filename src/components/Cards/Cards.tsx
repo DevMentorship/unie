fcbb2e4557c80a49e23972fc150e7f81e6a6bf42
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-05-07 18:58:43 +0400

    Clean up a bit Cards block

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -45,9 +45,10 @@ export const Cards = () => {
       <div className={styles.cards}>
         {cards.map(({ image, title }, index) => (
           <div key={index} className={styles.card}>
-            <Image className={styles.image} src={`/${image}.jpg`} alt={image} width={210} height={280} />
-            <div className={styles.background}></div>
-            <h3 className={styles.h3}>{title}</h3>
+            <Image className={styles.image} src={`/${image}.jpg`} alt={image} width={230} height={350} />
+            <div className={styles.inner}>
+              <h3 className={styles.h3}>{title}</h3>
+            </div>
           </div>
         ))}
       </div>
