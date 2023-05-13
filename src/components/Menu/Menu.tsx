c812564347117ac3e67c80aca1bbdaf84bb767a6
Liia Zubova <vardlitmo@gmail.com>
2023-05-13 14:37:43 +0300

    fix: conflicts

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,5 @@
 import cn from 'classnames';
 import Image from 'next/image';
-import { Key } from 'react';
 
 import styles from '@/components/Menu/Menu.module.css';
 
@@ -68,29 +67,3 @@ export const Menu = () => {
     </section>
   );
 };
-
-// import { urlFor } from '@/lib/client';
-
-// export interface IMenu {
-//   _id: string;
-//   mainImage: string;
-//   description: string;
-// }
-
-// export interface IProps {
-//   menu: IMenu[];
-// }
-
-// export const Menu = ({ menu }: IProps) => (
-//   <section>
-//     <h2>Menu</h2>
-//     <ul>
-//       {menu?.map(({ _id, mainImage, description }: IMenu) => (
-//         <li key={_id}>
-//           <Image src={urlFor(mainImage).url()} width="210" height="280" alt="" />
-//           <h2>{description}</h2>
-//         </li>
-//       ))}
-//     </ul>
-//   </section>
-// );
