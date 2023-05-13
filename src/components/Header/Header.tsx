12ed5c42f85c09853b3275776025656a606a235b
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-05-13 12:57:58 +0400

    Add active link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
+import cn from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import styles from '@/components/Header/Header.module.css';
 
@@ -9,46 +11,50 @@ const pages = [
   { label: 'Меню', href: '/menu' },
 ];
 
-export const Header = () => (
-  <header className="container">
-    <div className={styles.header}>
-      <div className={styles.blocks}>
-        <div className={styles.logo}>
-          <Link href="/">
-            <Image className={styles.image} src={'/logo.svg'} alt={'logo'} width={80} height={80} />
-          </Link>
-        </div>
+export const Header = () => {
+  const router = useRouter();
 
-        <div className={styles.contacts}>
-          <div className={styles.address}>
-            <Image className={styles.image} src={'/position.svg'} alt={'position'} width={25} height={25} />
-            <p>
-              <strong>Самара</strong>
-              <span className={styles.text}>Стара Загора 27</span>
-            </p>
+  return (
+    <header className="container">
+      <div className={styles.header}>
+        <div className={styles.blocks}>
+          <div className={styles.logo}>
+            <Link href="/">
+              <Image className={styles.image} src={'/logo.svg'} alt={'logo'} width={80} height={80} />
+            </Link>
           </div>
-          <div className={styles.phone}>
-            <Image className={styles.image} src={'/phone.svg'} alt={'phone'} width={25} height={25} />
-            <p>
-              <a href="tel:+79371846464">+7 (937) 184-64-64</a>
-            </p>
+
+          <div className={styles.contacts}>
+            <div className={styles.address}>
+              <Image className={styles.image} src={'/position.svg'} alt={'position'} width={25} height={25} />
+              <p>
+                <strong>Самара</strong>
+                <span className={styles.text}>Стара Загора 27</span>
+              </p>
+            </div>
+            <div className={styles.phone}>
+              <Image className={styles.image} src={'/phone.svg'} alt={'phone'} width={25} height={25} />
+              <p>
+                <a href="tel:+79371846464">+7 (937) 184-64-64</a>
+              </p>
+            </div>
           </div>
         </div>
+        <div className={styles.burger}>
+          <span></span>
+        </div>
+        <nav className={styles.menu}>
+          <ul className={styles.list}>
+            {pages.map(({ label, href }, index) => (
+              <li key={index} className={cn(router.pathname === href && styles.active)}>
+                <Link href={href} className={styles.link}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-      <div className={styles.burger}>
-        <span></span>
-      </div>
-      <nav className={styles.menu}>
-        <ul className={styles.list}>
-          {pages.map(({ label, href }, index) => (
-            <li key={index}>
-              <Link href={href} className={styles.link}>
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
