cbda31b26681a474da5a017656d6637b76961966
Anton Potapov <panton160786@gmail.com>
2023-05-23 22:30:55 +0930

    Other news connected to Sanity

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,57 +1,22 @@
-import Image from 'next/image';
+import { FC } from 'react';
 
 import styles from '@/components/Cards/Cards.module.css';
 
-export const Cards = () => {
-  const cards = [
-    {
-      image: 'matcha',
-      title: 'Матча или маття',
-    },
-    {
-      image: 'cans',
-      title: 'Потенциально опасный контент 18+',
-    },
-    {
-      image: 'vacancy',
-      title: 'Открытые вакансии',
-    },
-    {
-      image: 'coffee',
-      title: 'Дрип-кофе',
-    },
-    {
-      image: 'english',
-      title: 'English clubs',
-    },
-    {
-      image: 'soup',
-      title: 'Суп дня',
-    },
-    {
-      image: 'drinks',
-      title: 'Холодные напитки',
-    },
-    {
-      image: 'breakfast',
-      title: 'Завтраки',
-    },
-  ];
+import { Card } from '../Card/Card';
 
-  return (
-    <section className="container">
-      <h2 className={styles.h2}>Другие новости</h2>
+export interface INews {
+  _id: string;
+  title: string;
+  mainImage: string;
+}
 
-      <div className={styles.cards}>
-        {cards.map(({ image, title }, index) => (
-          <div key={index} className={styles.card}>
-            <Image className={styles.image} src={`/${image}.jpg`} alt={image} width={230} height={350} />
-            <div className={styles.inner}>
-              <h3 className={styles.h3}>{title}</h3>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
+export const Cards = ({ news }: { news: INews[] }) => (
+  <section className="container">
+    <h2 className={styles.h2}>Другие новости</h2>
+    <div className={styles.cards}>
+      {news.map((item: INews) => (
+        <Card key={item._id} {...item} />
+      ))}
+    </div>
+  </section>
+);
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,12 +1,15 @@
 import Head from 'next/head';
-import Link from 'next/link';
 
-import { Cards } from '@/components/Cards/Cards';
-import { client } from '@/lib/client';
-
+import { Cards, INews } from '../components/Cards/Cards';
+import { client } from '../lib/client';
 import type { IPost } from './post/[slug]';
 
-const Posts = ({ posts }: { posts: IPost[] }) => (
+interface IProps {
+  posts: IPost[];
+  news: INews[];
+}
+
+const Posts = ({ posts, news }: IProps) => (
   <>
     <Head>
       <title>Unie - Новости</title>
@@ -17,14 +20,13 @@ const Posts = ({ posts }: { posts: IPost[] }) => (
           <h2>{post.title}</h2>
           <p>{post.publishedAt}</p>
           <p>{post.description}</p>
-          <Link href={`/post/${post.slug.current}`}>
+          {/* <Link href={`/post/${post.slug.current}`}>
             <span>Подробнее</span>
-          </Link>
+          </Link> */}
         </div>
       ))}
     </section>
-
-    <Cards />
+    <Cards news={news} />
   </>
 );
 
@@ -33,14 +35,16 @@ export default Posts;
 export const getStaticProps = async () => {
   const query = `{
     "posts": *[_type == "post"] | order(publishedAt desc)  {_id, publishedAt, title, description, mainImage, slug},
+    "news": *[_type == "news"] | order(number asc) {_id, title, mainImage}
   }`;
 
-  const { posts: result } = await client.fetch(query);
-
-  const posts = result.map((post: IPost) => ({
+  const result = await client.fetch(query);
+  const posts = result.posts.map((post: IPost) => ({
     ...post,
     publishedAt: new Date(post.publishedAt).toLocaleString('default', { month: 'short', day: 'numeric' }),
   }));
 
-  return { props: { posts } };
+  const news = result.news;
+
+  return { props: { posts, news } };
 };
